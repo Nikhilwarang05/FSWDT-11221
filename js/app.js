e81ad601ce564bd3fad358bd8d845bd39638e089
Nikhilwarang05 <nikhilajitwarang05@gmail.com>
2025-01-10 11:10:14 +0530

    primitive data types

---
+++ js/app.js
@@ -0,0 +1,18 @@
+// // Number data type
+// let age = 22
+// // String
+// let brand ='xoxo'
+// console.log(brand)
+
+// let age 
+// console.log(age)
+
+
+console.log(typeof 22)
+console.log(typeof 22.023420)
+console.log(typeof -22)
+console.log(typeof 'c')
+console.log(typeof 'string')
+console.log(typeof true)
+console.log(typeof null)
+console.log(typeof undefined)
